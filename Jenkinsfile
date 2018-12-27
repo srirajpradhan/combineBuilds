617a2fe8d738d375c8d025e28730b55513907968
Sriraj Pradhan <sriraj.pradhan@relevancelab.com>
2018-12-27 15:37:10 +0530

    jenkins trial

--- Jenkinsfile
+++ Jenkinsfile
@@ -3,15 +3,29 @@ pipeline {
   enviroment {
     IMAGE_NAME = ""
   }
+  parameters {
+   choice(name: 'CHOICE', choices: ['Initalize', 'Operations'], description: 'Pick something')
+  }
   stages {
-    stage('Initialize System') {
+    stage('Choice') {
+     echo "Choice: ${params.CHOICE}"
+    }
+   /*  stage('Initialize System') {
+      when {
+	expression { ${params.CHOICE}}
+      }
       steps {
         try {
-
+          sh './provision.sh'
         } catch(err) {
           echo 'Aborted Due to ${err}'
         }
       }
+    } */
+  }
+  post {
+    always {
+	echo 'End of File!!!!!!!!!!!!!!'
     }
   }
 }
--- provision.sh
+++ provision.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
 
-java -version
-lsb_release -a
+FILE="Vagrantfile"
+
+mkdir -p vm
+cp first.sh vm/
+cd vm
+
+set +e
+
+IP=vagrant ssh -c "hostname -I | cut -d' ' -f2" 2>/dev/null
+echo "IP Address $IP"
+if [ ! -f $FILE ]; then
+	echo "File Does not Exist"
+        vagrant init geerlingguy/ubuntu1804
+	sed -i -e 's/# config.vm.network "private_network"/ config.vm.network "private_network"/' $FILE
+fi
+
+vagrant up
+
+# Copying Script in Vagrant Machine
+
+scp -o StrictHostKeyChecking=no -i /home/rle0572/vault/src/project/deployScript/vm/.vagrant/machines/default/virtualbox/private_key first.sh vagrant@$(awk 'BEGIN { FS = ", ip:"}{print $2}' Vagrantfile | grep '"'| cut -d '"' -f 2):~/
+
+vagrant ssh -c "./first.sh"
+echo "outside: $HOME"
+
+set -e
+
